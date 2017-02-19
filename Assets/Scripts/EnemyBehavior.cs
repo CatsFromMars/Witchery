90ec472615f365757825d180a14b5de603457647
avshanka@andrew.cmu.edu <avshanka@andrew.cmu.edu>
2017-02-19 16:19:45 -0500

    Enemy Cube

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+    [SerializeField]
+    float speed = 5;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Started!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("End");
+        transform.Translate(transform.forward * Time.deltaTime * speed);
+	}
+}
